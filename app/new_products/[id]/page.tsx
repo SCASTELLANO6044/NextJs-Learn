65b284c4590879829d03b51b9a181cf52efdd372
Sergio Castellano Betancor <scastellanobetancor@gmail.com>
2025-12-29 18:12:31 +0000

    style

--- app/new_products/[id]/page.tsx
+++ app/new_products/[id]/page.tsx
@@ -2,11 +2,13 @@ export async function generateStaticParams() {
     return [{id: "1"}, {id: "2"}, {id: "3"}]
 }
 
-export default async function ProeductPage({
+export default async function ProeductPage(
+{
     params,
 }:{
     params: Promise<{id: string}>
-}) {
+}
+) {
     const {id} = await params
     return(
         <h1>
