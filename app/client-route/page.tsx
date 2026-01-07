7f4802acbe854205e158137af1dd69a9db49187e
Sergio Castellano Betancor <scastellanobetancor@gmail.com>
2026-01-07 18:34:15 +0000

    :zap: Client-only Code

--- app/client-route/page.tsx
+++ app/client-route/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 
 import { serverSideFunction } from "../utils/server-utils";
+import { clientSideFunctions } from "../utils/client-utils";
 
 export default function CñientRoutePage(){
     const result = serverSideFunction();
+    const client = clientSideFunctions();
     return(
-        <h1>Client Route {result}</h1>
+        <>
+            <h1>Client Route {result}</h1>
+            <p>{client}</p>
+        </>
     );
 }
