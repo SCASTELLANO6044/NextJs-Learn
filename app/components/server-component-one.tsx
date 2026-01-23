e7914533fc0fae2a0bc5c7c82cd75205fb865137
Sergio Castellano Betancor <sergiocastellanotrabajo@gmail.com>
2026-01-23 20:05:18 +0100

    :zap: Client component inside another Server component

--- app/components/server-component-one.tsx
+++ app/components/server-component-one.tsx
@@ -1,6 +1,6 @@
 import fs from "fs"
 
-import { ServerComponentTwo } from "./server-component-two";
+import { ClientComponentOne } from "./client-component-one";
 
 export const ServerComponentOne = () =>{
     fs.readFileSync("app/components/server-component-one.tsx", "utf-8")
@@ -8,7 +8,7 @@ export const ServerComponentOne = () =>{
     return (
         <>
             <h1>Server component one.</h1>
-            <ServerComponentTwo/>
+            <ClientComponentOne/>
         </>
     );
 };
--- app/interleaving/page.tsx
+++ app/interleaving/page.tsx
@@ -1,11 +1,11 @@
 import fs from "fs"
-import { ClientComponentOne } from "../components/client-component-one";
+import { ServerComponentOne } from "../components/server-component-one";
 
 export default function InterleavingPage(){
     return(
         <>
             <h1>Interleaving page.</h1>
-            <ClientComponentOne/>
+            <ServerComponentOne/>
         </>
     );
 }
