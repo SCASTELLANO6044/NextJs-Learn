8b78e46d19cf50510f1a7c291525e23567dc1a71
Sergio Castellano Betancor <sergiocastellanotrabajo@gmail.com>
2026-01-23 20:35:19 +0100

    :zap: Fetching Data in Client Components

--- app/interleaving/page.tsx
+++ app/interleaving/page.tsx
@@ -2,10 +2,10 @@ import fs from "fs"
 import { ClientComponentOne } from "../components/client-component-one";
 
 export default function InterleavingPage(){
-    return(
-        <>
-            <h1>Interleaving page.</h1>
-            <ClientComponentOne/>
-        </>
-    );
+    // return(
+    //     <>
+    //         <h1>Interleaving page.</h1>
+    //         <ClientComponentOne/>
+    //     </>
+    // );
 }
